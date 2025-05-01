cff6c580aee5488d628166e612669e91170f691b
Fink2005 <phans3806@gmail.com>
2025-05-01 22:58:09 +0700

    change location path

--- config.php
+++ config.php
@@ -1,13 +1,12 @@
 <?php
 use Cloudinary\Configuration\Configuration;
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2));
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__); 
 $dotenv->safeLoad();
 Configuration::instance([
-    
 'cloud' => [
-'cloud_name' => $_ENV['CLOUDINARY_CLOUD_NAME'],
-'api_key' => $_ENV['CLOUDINARY_API_KEY'],
-'api_secret' => $_ENV['CLOUDINARY_API_SECRET'],
+'cloud_name' => $_ENV['CLOUDINARY_CLOUD_NAME'] ,
+'api_key' => $_ENV['CLOUDINARY_API_KEY'] ,
+'api_secret' => $_ENV['CLOUDINARY_API_SECRET'] 
 ],
 'url' => ['secure' => true]
 ]);
